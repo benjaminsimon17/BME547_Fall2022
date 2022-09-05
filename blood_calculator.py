bbe95e0859351584205bd4c1176636d6e87237ca
benjaminsimon17 <benjamin.simon@duke.edu>
2022-09-05 12:30:58 -0400

    wrote basic framework for input user

---
+++ blood_calculator.py
@@ -0,0 +1,12 @@
+def interface():
+    print("BLood Calculator")
+    print("Options:")
+    print("9 - Quit")
+    choice = input("Enter choice:")
+    if choice == "9":
+        return 
+
+
+
+
+interface()
