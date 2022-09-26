43363ae4fdd75e13fae6cd7591e5580c377caa5e
benjaminsimon17 <benjamin.simon@duke.edu>
2022-09-26 18:26:35 -0400

    converted database from list of patients to dictionary of patients with id as key

--- database.py
+++ database.py
@@ -17,7 +17,7 @@ def print_database(db):
     for patient in db:
         print(patient)
         print("Name: {}, id: {}, age: {}".
-              format(get_full_name(patient), patient["ID"], patient["Age"]))
+              format(get_full_name(db[patient]), db[patient]["ID"], db[patient]["Age"]))
 
 
 def get_full_name(patient):
@@ -39,22 +39,20 @@ def adult_or_minor(patient):
 
 
 def find_patient(db, id_number):
-    for patient in db:
-        if patient["ID"] == id_number:
-            return patient
-    return False
+    patient = db[id_number]
+    return patient
 
 
 def main():
-    db = []
-    db.append(create_patient_entry("Ann", "Ables", 1, 30))
-    db.append(create_patient_entry("Bob", "Boyles", 2, 34))
-    db.append(create_patient_entry("Chris", "Chou", 3, 25))
+    db = {}
+    db[11] = create_patient_entry("Ann", "Ables", 11, 30)
+    db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
+    db[3] = create_patient_entry("Chris", "Chou", 3, 25)
     print_database(db)
     test_result_adder(db, 3, "HDL", 100)
     print_database(db)
-    print("Patient {} is a {}".format(get_full_name(db[2]),
-                                      adult_or_minor(db[2])))
+    # print("Patient {} is a {}".format(get_full_name(db[2]),
+    #                                   adult_or_minor(db[2])))
 
 
 if __name__ == '__main__':
