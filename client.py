f471f77330305b6039ed2014bf6b86326b6692ba
benjaminsimon17 <benjamin.simon@duke.edu>
2022-10-19 13:56:59 -0400

    completed client/server flask classwork

---
+++ client.py
@@ -0,0 +1,19 @@
+import requests
+# r = requests.get("http://127.0.0.1:5000/info")
+
+# print(r.status_code)
+# print(r.text)
+
+
+# out_data = {"name": "Benjamin Simon",
+#             "hdl_value": 300}
+# r = requests.post("http://127.0.0.1:5000/hdl_check", json = out_data)
+# print(r.status_code)
+# print(r.text)
+
+
+out_data = {"a": 50,
+            "b": 11}
+r = requests.post("http://127.0.0.1:5000/add_numbers", json=out_data)
+print(r.status_code)
+print(r.text)
