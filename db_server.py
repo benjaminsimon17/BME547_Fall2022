80c502335b14b13cf41811093042f642cafecbb4
benjaminsimon17 <benjamin.simon@duke.edu>
2022-10-26 19:56:30 -0400

    completed classwork on october 26th

--- db_server.py
+++ db_server.py
@@ -46,6 +46,61 @@ def add_new_patien_worker(in_data):
     return "Patient succesfully added", 200
 
 
+@app.route("/new_patient", method=["POST"])
+def new_patient():
+    in_data = request.get_json()
+    add_new_patien_worker(in_data)
+    return "Patient succesfully added", 200
+
+
+@app.route("/get_results/<patient_id>", methods=["GET"])
+def get_results(patient_id):
+    for i in db:
+        if i["id"] == patient_id:
+            return i["test_result"]
+        else:
+            return "Patient not found"
+
+
+@app.route("/add_test", method=["POST"])
+def add_test_flask_handler():
+    in_data = request.get_json()
+    msg, status_code = add_test_worker(in_data)
+    return msg, status_code
+
+
+def add_test_worker(in_data):
+    msg = add_test_validation(in_data)
+    if msg is not True:
+        return msg, 400
+    add_test_to_patient(in_data)
+    return "Test added", 200
+
+
+def add_test_to_patient(in_data):
+    patient = find_patient(in_data["id"])
+    patient["test_name"].append(in_data["test_name"])
+    patient["test_result"].append(in_data["test_result"])
+
+
+def add_test_validation(in_data):
+    expected_keys = ["id", "test_name", "test_result"]
+    expected_types = [int, str, int]
+    for ex_key, ex_type in zip(expected_keys, expected_types):
+        if ex_key not in in_data:
+            return "Key missing"
+        if type(in_data[ex_key]) is not ex_type:
+            return "Bad Value Type"
+    return True
+
+
+def find_patient(patient_id):
+    for patient in db:
+        if patient["id"] == patient_id:
+            return patient
+    return False
+
+
 def validate_new_patient_info(in_data):
     if type(in_data) is not dict:
         return "POST data was not a dictionary"
